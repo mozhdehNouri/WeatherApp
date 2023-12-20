4e01482049870e6937929af73bcc789653bf6d3d
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2023-12-20 09:26:12 +0330

    Init project (#Master)

---
+++ settings.gradle.kts
@@ -0,0 +1,15 @@
+pluginManagement {
+    repositories {
+
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+
+    }
+}
+
+rootProject.name = "TodoAppWithCleanArchitecture"
+include(":app")
+ 
