608ca3851117de9c6abb6752db19389224ccfdbd
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2024-01-15 16:57:52 +0330

    Update Dependency (#master)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,12 +1,28 @@
 pluginManagement {
     repositories {
-
+        maven {
+            url =
+                uri("https://inexus.samentic.com/repository/samentic-android")
+            artifactUrls("https://inexus.samentic.com/repository/samentic-android")
+            credentials {
+                username = "mozhdeh.nouri"
+                password = "Av48,HZh%q4=S'2"
+            }
+        }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-
+        maven {
+            url =
+                uri("https://inexus.samentic.com/repository/samentic-android")
+            artifactUrls("https://inexus.samentic.com/repository/samentic-android")
+            credentials {
+                username = "mozhdeh.nouri"
+                password = "Av48,HZh%q4=S'2"
+            }
+        }
     }
 }
 
