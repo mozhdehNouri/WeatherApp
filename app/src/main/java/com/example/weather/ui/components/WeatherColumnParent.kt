6d9a1bb4f2934c6ed50c3d3ce1f58177de4cee1b
mozhdeh.nouri <mozhdehnouri@yahoo.com>
2024-06-29 16:04:44 +0330

    Some Improvement in ForecastUI (#8)

--- app/src/main/java/com/example/weather/ui/components/WeatherColumnParent.kt
+++ app/src/main/java/com/example/weather/ui/components/WeatherColumnParent.kt
@@ -1,28 +1,30 @@
 package com.example.weather.ui.components
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 @Composable
 inline fun WColumn_Parent(
     header: @Composable () -> Unit,
     content: @Composable ColumnScope.() -> Unit,
     bottom: @Composable () -> Unit,
+    backgroundColor: List<Color>
 ) {
     val columnPadding = LocalColumnParentPadding.current
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(columnPadding)
-            .background(MaterialTheme.colorScheme.background),
+            .horizontalGradientBackground(
+                backgroundColor
+            )
+            .padding(columnPadding),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
